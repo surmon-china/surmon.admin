93edc279ffa5a94baefdc4360dabaa653f5e5360
Surmon <surmon.me@gmail.com>
2024-05-18 02:23:52 +0800

    feat: improve 404 page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -5,20 +5,23 @@
 
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Result, Button } from 'antd'
+import { Flex, Result, Button } from 'antd'
+import { Trans } from '@/i18n'
 
 import styles from './style.module.less'
 
 export const NotFoundPage: React.FC = () => (
-  <div className={styles.notFound}>
+  <Flex justify="center" align="center" className={styles.notFound}>
     <Result
       status="warning"
       title="404 NOT FOUND"
       extra={
         <Link to="/">
-          <Button type="link">Go Home</Button>
+          <Button type="link">
+            <Trans i18nKey="page.404.link" />
+          </Button>
         </Link>
       }
     />
-  </div>
+  </Flex>
 )
